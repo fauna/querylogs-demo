cedfe1e057ff30906aefbaf456532289450ad15a
Cleve Stuart <90649124+cleve-fauna@users.noreply.github.com>
2023-02-15 12:10:09 -0800

    Example program for fetching query logs (#7)
    * Example program with account keys
    
    * README fix up
    
    * Update README.md
    
    * Update example-program.js
    
    * improve demo time bounds

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# What is this?
-
-A demo application for receiving query logs from your preview account.
-
-Presently it uses your username and password to create a synthetic dashboard session enabling the program to receive logs; however, when
-the product launches we will support the creation and usage of API tokens. This will enable programs to use API tokens, rather than synthetic
-dashboard sessions, to receive query logs.
-
-# How do I use it?
+# Setup
 
 1. Install dependencies: `npm install`
-2. Run the demo application: `npm run demo`
 
-The demo application walks you through requesting Fauna query logs.
-Once query logs are received, you have the option to download them
-locally for analysis.
+# What is this?
+
+A demo application for receiving query logs from your preview account or production account.
+
+It runs in two modes:
+
+1. **Demo Mode** `npm run demo` - a guided CLI that uses the username and password of your preview account to create a synthetic dashboard session enabling the program to receive logs. The CLI demo is intended to show you the core functionality offered.
+2. **Program Mode** - `npm run example-program` - uses an **Account Key** to call the Fauna's frontdoor service to requests logs for your production Fauna account. To use this, make an **Account Key** in the production Fauna account of your choice, export it to your local environment with `export ACCOUNT_KEY=<your_key>` and run! Inspect the source code in `example-program.js` to tweak the inputs.
+
---
+++ example-program.js
@@ -0,0 +1,76 @@
+const axios = require('axios').default;
+const winston = require('winston');
+const lux = require("luxon");
+
+const frontdoorClient = axios.create({
+  baseURL: "https://frontdoor.fauna.com",
+  timeout: 10000,
+});
+
+/**
+   This example program shows you how to use an Account Key to fetch
+   logs programmatically. You could extend such a program to act on any
+   region group or time-bad according to your input; and extend it further
+   to fetch the logs with the resulting presigned_url and then dump it into
+   your observability platform.
+
+   To use it set ACCOUNT_KEY environmental variable to an account key in
+   your Fauna account.
+*/
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'querylogs-demo' },
+  transports: [
+    new winston.transports.Console({
+      level: "debug",
+      handleExceptions: true,
+      // enable json output, good to send to DD
+      format: winston.format.json(),
+    }),
+  ],
+});
+
+const today = lux.DateTime.now().toISO();
+const yesterday = lux.DateTime.now().minus(lux.Duration.fromISO("P1D")).toISO();
+
+async function getLogs() {
+  const headers = { Authorization: `Bearer ${process.env["ACCOUNT_KEY"]}` };
+  const { data: querylogRequest } = await frontdoorClient.post(
+    "/api/v1/logs?type=query",
+    { region_group: "us-std", time_start: yesterday, time_end: today},
+    { headers }
+  );
+  logger.info(querylogRequest);
+  await pollResults(querylogRequest, headers, "us-std");
+}
+
+if (process.env["ACCOUNT_KEY"] === undefined) {
+  logger.error("You must set ACCOUNT_KEY in your local environment to run this program!");
+  return;
+}
+
+async function pollResults(
+  querylogRequest,
+  headers,
+  region_group,
+) {
+  let result;
+  const maxRuntimeMs = 300 * 1000;
+  const time_start = Date.now();
+  do {
+    ({ data: result } = await frontdoorClient.get(
+      `/api/v1/logs/${querylogRequest.request_id}?regionGroup=${region_group}&type=query`,
+      { headers }
+    ));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    logger.info(`State: ${result.state}`);
+  } while (
+    Date.now() < time_start + maxRuntimeMs &&
+    !["Complete", "DoesNotExist", "Failed", "TimedOut"].includes(result.state)
+  );
+  logger.info(result);
+  return result;
+}
+
+getLogs().then(() => logger.info("Thanks for trying out Fauna logs! Please give us any and all feedback!"));
