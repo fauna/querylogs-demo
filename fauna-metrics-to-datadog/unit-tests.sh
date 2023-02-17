7d136b320201c296c7a5ebadf5a8622f3417602d
Cleve Stuart <cleve.stuart@fauna.com>
2023-02-17 11:46:46 -0800

    Include vector example in Demo

---
+++ fauna-metrics-to-datadog/unit-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$DD_API_KEY" ]; then
+  echo "Define env var DD_API_KEY with datadog api key"
+  exit 2;
+fi
+
+export LOG_DIR=$1
+
+if [ -z "$LOG_DIR" ]; then
+  echo "usage: run-vector-snowflake-in-docker.sh [path_to_directory_containing_log_files]"
+  exit 2;
+fi
+
+# run unit tests
+docker run -i \
+  -e DD_API_KEY=$DD_API_KEY \
+  -v $(pwd)/vector.yml:/etc/vector/vector.yml \
+  -v $(pwd)/$LOG_DIR:/var/log/querylog -v $(pwd)/$LOG_DIR/../vector-state:/var/lib/vector \
+  -v $(pwd)/vector.console_output.yml:/etc/vector/vector.console_output.yml --rm timberio/vector:0.24.1-debian test /etc/vector/vector.yml
